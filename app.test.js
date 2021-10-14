81ec0eefb9e9f3e10253d25d5007a747896c201d
Sasha <sasha.luhr@gmail.com>
2021-10-15 01:24:05 +0200

    tests working

--- app.test.js
+++ app.test.js
@@ -3,10 +3,9 @@ import app from "./app.js";
 
 describe('GET request', () => {
 
-    test('should return "Hello"', () => {
-        const response = request(app).get('/');
-        expect(response.statusCode).toBe(200);
-        response.end();
+    test('should return "status 200"', async () => {
+        const response = await request(app).get('/');
+        expect(response.statusCode).toEqual(200);
     });
 
 });
